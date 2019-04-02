eb6296f530bc9ad7f519d362ad868b44e6026064
Jeiv Septian Rahmatullah <48686850+pusaykillah@users.noreply.github.com>
2019-04-02 08:53:09 +0700

    Update JavaApplication1.java

--- JavaApplication1/src/javaapplication1/JavaApplication1.java
+++ JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -12,27 +12,38 @@ import java.util.Scanner;
  * @author Praktikan
  */
 public class JavaApplication1 {
+    static int calculateSum(int n) 
+    { 
+        if (n <= 0) 
+           return 0; 
+        int fibo[]=new int[n+1]; 
+        fibo[0] = 0; fibo[1] = 1; 
+       
+        // Initialize result 
+        int sum = fibo[0] + fibo[1]; 
+       
+        // Add remaining terms 
+        for (int i=2; i<=n; i++) 
+        { 
+            fibo[i] = fibo[i-1]+fibo[i-2]; 
+            sum += fibo[i]; 
+        }
+        return sum; 
+    } 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
        
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Input : ");
-        int a = scan.nextInt();
-        long fibonaci[] = new long[a];
-           System.out.println("Output : ");
-        fibonaci[0] = 0;
-        fibonaci[1] = 1;
-         
-        for(int i = 2; i < a; i++) {
-            fibonaci[i] = fibonaci[i-1] + fibonaci[i-2];
-        }
-         
-        for (int i = 0; i < a; i++) {
-            System.out.println(fibonaci[i] +  " ");
-        }
+        int n = 0;
+            Scanner scan = new Scanner(System.in);
+            System.out.print("input: ");
+            n = scan.nextInt();
+            
+            System.out.println(" ");
+            System.out.println(calculateSum(n));
+            System.out.println(" ");
+            
+	}
     }
- 
-}
