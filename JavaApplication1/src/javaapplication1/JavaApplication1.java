933bac2482227eb01e6f746a5b3c06489e6d971d
Jeiv Septian Rahmatullah <48686850+pusaykillah@users.noreply.github.com>
2019-04-02 08:41:32 +0700

    Add files via upload

---
+++ JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+
+import java.util.Scanner;
+/**
+ *
+ * @author Praktikan
+ */
+public class JavaApplication1 {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Input : ");
+        int a = scan.nextInt();
+        long fibonaci[] = new long[a];
+           System.out.println("Output : ");
+        fibonaci[0] = 0;
+        fibonaci[1] = 1;
+         
+        for(int i = 2; i < a; i++) {
+            fibonaci[i] = fibonaci[i-1] + fibonaci[i-2];
+        }
+         
+        for (int i = 0; i < a; i++) {
+            System.out.println(fibonaci[i] +  " ");
+        }
+    }
+ 
+}
